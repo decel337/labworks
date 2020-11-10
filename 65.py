2f39318e00261e2a9b3ecba50ff3549d7b2fafdf
decel337 <gengshit228@gmail.com>
2020-11-10 22:40:20 +0200

    lab3

---
+++ 65.py
@@ -0,0 +1,16 @@
+x = float(input("Input value x: "))
+n = 1
+
+
+def p(x, n):
+    return (((-1) ** (n - 1)) * ((x - 1) ** n)) / n
+
+
+sum = p(x, n)
+if 0 < x < 2:
+    while abs(p(x, n + 1) - p(x, n)) > 10 ** (-6):
+        n += 1
+        sum += p(x, n)
+    print("Your function value:", round(sum + p(x, n + 1), 6))
+else:
+    print("ОШИБКА! Введите число х є (0;2)")
